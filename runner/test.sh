68eeff798ff8beba081f338dc31a59869ab34f88
notakamilice <dasha290193@mail.ru>
2015-03-26 02:14:31 +0400

    for tests

---
+++ runner/test.sh
@@ -0,0 +1,12 @@
+echo "Testing uniform" >> 1
+make test_uni 2>> 1
+make test_uni 2>> 1
+make test_uni 2>> 1
+echo "Testing oldest" >> 1
+make test_old 2>> 1
+make test_old 2>> 1
+make test_old 2>> 1
+echo "Testing latest" >> 1
+make test_lat 2>> 1
+make test_lat 2>> 1
+make test_lat 2>> 1
