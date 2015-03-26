1fe73d716d08e868ae0b18dfa15d6a138b788787
notakamilice <dasha290193@mail.ru>
2015-03-26 17:12:33 +0400

    add mydb.h seems to be correct

---
+++ mydb.h
@@ -0,0 +1,18 @@
+#include <stddef.h>
+#include <iostream>
+#include <stdio.h>
+#include "main_data_structures.h"
+#include "work_with_blocks.h"
+
+/* Open DB if it exists, otherwise create DB */
+struct DB *dbopen(char *file, struct DBC *conf);
+
+struct DB *dbcreate(char *file, struct DBC *conf);
+
+int db_close(struct DB *db);
+int db_delete(struct DB *, void *, size_t);
+int db_select(struct DB *, void *, size_t, void **, size_t *);
+int db_insert(struct DB *, void *, size_t, void * , size_t  );
+
+/* Sync cached pages with disk */
+int db_sync(const struct DB *db);
